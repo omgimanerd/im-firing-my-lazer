601f2eab17864533a23181fa44e42421f6fc454e
omgimanerd <alvin.lin.dev@gmail.com>
2016-04-11 14:28:45 -0400

    we don't need this

--- Scanner/Scanner.ino
+++ Scanner/Scanner.ino
@@ -1,23 +1,14 @@
-int values[10];
-int index = 0;
+/**
+ * Base code for Arduino.
+ */
+
+// Globals
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  
+  // initialize serial communication at 9600 bits per second
+  Serial.begin(9600);  
 }
 
 void loop() {
-  if(index<10){
-      values[index] = analogRead(A0);
-      index++;
-  }else{
-      int total = 0;
-      for(index = 0;index<10;index++){
-         total+=values[index];
-      }
-      index = 0;
-      Serial.println(total/10);
-      delay(100);
-  }
-  
+  Serial.println(analogRead(A0));
 }

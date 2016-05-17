c7b137321bb8dabf2ccab2f9c5fc1cf94c165655
omgimanerd <alvin.lin.dev@gmail.com>
2016-05-17 14:50:58 -0400

    added gradient image generation

--- scanner.py
+++ scanner.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
-from graphics.graphics.lib.color import Color
-from graphics.graphics.lib.drawing import Drawing
+from graphics.lib.color import Color
+from graphics.lib.drawing import Drawing
+from graphics.lib.util import Util
 
+import argparse
 import json
-import math
 import serial
 import sys
 import time
@@ -42,27 +43,43 @@ class Scanner():
         return data
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "Usage: python scanner.py <serial location>"
-        print "Example: python scanner.py /dev/ttyACM0"
-        sys.exit(0)
-    print "Reading from %s" % sys.argv[1]
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("serial_port", help="The serial port to read from")
+    argparser.add_argument("type", help="The type of data visualization,"
+                           "either graph or gradient")
+    args = argparser.parse_args()
 
-    scanner = Scanner.create(sys.argv[1])
+    if args.type not in ['graph', 'gradient']:
+        raise ValueError("%s is not a valid data visualization" % args.type)
 
+    print "Generating a %s of scanner data..." % args.type
+    scanner = Scanner.create(args.serial_port)
     data = scanner.read_laser_info()
-    print "Generating image..."
 
-    width = len(data)
-    height = int(max(data) * 2) + 20
-    drawing = Drawing(width, height)
-    for i in range(len(data) - 1):
-        drawing.draw_line(i, height - int(data[i]) - 10, 0,
-                          i + 1, height - int(data[i + 1]) - 10, 0,
-                          Color.BLACK())
+    print "Generating %s..." % args.type
+
+    drawing = None
+    if args.type == "graph":
+        width = len(data)
+        height = int(max(data) * 2) + 20
+        drawing = Drawing(width, height)
+        for i in range(len(data) - 1):
+            drawing.draw_line(i, height - int(data[i]) - 10, 0,
+                              i + 1, height - int(data[i + 1]) - 10, 0,
+                              Color.BLACK())
+    elif args.type == "gradient":
+        width = len(data)
+        height = 100
+        drawing = Drawing(width, height)
+        low, high = min(data), max(data)
+        
+        for i in range(len(data) - 1):
+            scale = Util.linear_scale(data[i], low, high, 0, 255)
+            color = Color([scale, scale, scale])
+            drawing.draw_line(i, 0, 0, i, 99, 0, color)
+
     filename = "%s" % time.strftime("%m-%d-%Y_%H-%M-%S")
     drawing.display()
-
     print "Type 'save' to save, otherwise type anything to exit"
     if raw_input() == "save":
         drawing.generate("data/png/%s" % filename, extension="png")
